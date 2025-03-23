413f1a2043e13367cd42d4364af1be0e6fda35b1
Shawn McCoy <smccoy@nationalparcel.com>
2025-03-23 12:41:44 -0400

    init

---
+++ NetDaemonApps/HomeAssistantGenerated.cs
@@ -0,0 +1,24 @@
+//------------------------------------------------------------------------------
+// This is a placeholder for the HomeAssistantGenerated.cs file that will be generated when running nd-codegen 
+//
+// To update this file with latest entities run this command in your project directory:
+//   dotnet tool run nd-codegen
+//
+// In the template projects we provided a convenience powershell script that will update
+// the codegen and nugets to latest versions update_all_dependencies.ps1.
+//
+// For more information: https://netdaemon.xyz/docs/user/hass_model/hass_model_codegen
+// For more information about NetDaemon: https://netdaemon.xyz/
+//------------------------------------------------------------------------------
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeAssistantGenerated;
+public static class GeneratedExtensions
+{
+    ///<summary>Dummy implementation of AddHomeAssistantGenerated()</summary>
+    ///<remarks>Run nd-codegen to overwrite this file</remarks>
+    public static IServiceCollection AddHomeAssistantGenerated(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection;
+    }
+}
