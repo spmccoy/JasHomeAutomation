effbf00f4b7dc3edd1ae58ff141e845c45801f54
spmccoy <48337525+spmccoy@users.noreply.github.com>
2025-04-30 13:57:02 -0400

    Refactor MacBookPro app to use BinarySensorEntities. (#23)
    Replaced SensorEntities with BinarySensorEntities for clarity and updated logic to match the new API. Simplified state handling using a switch statement for better readability and maintainability.
    
    Co-authored-by: spmccoy <smccoy@nationalparcel.com>

--- NetDaemonApps/apps/ShawnRoom/MacBookPro.cs
+++ NetDaemonApps/apps/ShawnRoom/MacBookPro.cs
@@ -7,41 +7,33 @@ namespace NetDaemonApps.apps.ShawnRoom;
 [NetDaemonApp]
 public class MacBookPro
 {
-    private readonly IPersonService _personService;
-    private readonly SwitchEntities _switches;
-    private readonly ILogger _logger;
-
-    public MacBookPro(IPersonService personService, SensorEntities sensors, SwitchEntities switches, ILogger<MacBookPro> logger)
+    public MacBookPro(IPersonService personService, BinarySensorEntities binarySensors, SwitchEntities switches, ILogger<MacBookPro> logger)
     {
-        _personService = personService;
-        _switches = switches;
-        _logger = logger;
-
-        sensors.ShawnsMacbookProActiveAudioInput
+        binarySensors.ShawnsMacbookProAudioInputInUse
             .StateChanges()
-            .Subscribe(ProcessStateChange);
-    }
-
-    private void ProcessStateChange(StateChange<SensorEntity, EntityState<SensorAttributes>> stateChange)
-    {
-        var newState = stateChange.New?.State;
-        var oldState = stateChange.Old?.State;
+            .Subscribe(stateChange =>
+            {
+                var newState = stateChange.New?.State;
+                var oldState = stateChange.Old?.State;
         
-        _logger.LogInformation("State changed from {oldState} to {newState}", oldState, newState);
+                logger.LogInformation("State changed from {oldState} to {newState}", oldState, newState);
 
-        if (!_personService.ShawnHome)
-        {
-            _logger.LogDebug("Short circuit, shawn is not home");
-            return;
-        }
+                if (!personService.ShawnHome)
+                {
+                    logger.LogDebug("Short circuit, shawn is not home");
+                    return;
+                }
         
-        if (newState == HaState.Active)
-        {
-            _switches.ShawnroomDndNetdaemon.TurnOn();
-        }
-        else if (newState == HaState.InActive)
-        {
-            _switches.ShawnroomDndNetdaemon.TurnOff();
-        }
+                switch (newState)
+                {
+                    case HaState.On:
+                        switches.ShawnroomDndNetdaemon.TurnOn();
+                        break;
+                    
+                    case HaState.Off:
+                        switches.ShawnroomDndNetdaemon.TurnOff();
+                        break;
+                }
+            });
     }
 }
