f6422197f31530fa6fe889488a5e2845f0971376
spmccoy <48337525+spmccoy@users.noreply.github.com>
2025-05-01 18:16:58 -0400

    Refactor camera notification handling logic. (#31)
    Update `SilenceCameraNotificationsSwitch` to improve scheduling and add a room state check before turning notifications back on. Enhance `HouseService` to manage camera notifications during specific house states for better automation consistency.
    
    Co-authored-by: spmccoy <smccoy@nationalparcel.com>

--- NetDaemonApps/apps/House/SilenceCameraNotificationsSwitch.cs
+++ NetDaemonApps/apps/House/SilenceCameraNotificationsSwitch.cs
@@ -1,4 +1,5 @@
 using System.Reactive.Concurrency;
+using MqttEntities.Models;
 using NetDaemonApps.Models;
 
 namespace NetDaemonApps.apps.House;
@@ -12,29 +13,28 @@ public class SilenceCameraNotificationsSwitch
     
     private IDisposable? _schedulerSubscription;
     
-    public SilenceCameraNotificationsSwitch(SwitchEntities switches, IScheduler scheduler)
+    public SilenceCameraNotificationsSwitch(SwitchEntities switches, IScheduler scheduler, SelectEntities selects)
     {
         _switches = switches;
         _scheduler = scheduler;
         switches.HouseCameraNotificationsNetdaemon.StateChanges()
             .Where(w => w.New?.State == HaState.On)
-            .Subscribe(_ => HandleOn());
+            .Subscribe(_ => Helpers.CancelSchedule(_schedulerSubscription));
         
         switches.HouseCameraNotificationsNetdaemon.StateChanges()
             .Where(w => w.New?.State == HaState.Off)
-            .Subscribe(_ => HandleOff());
-    }
-    
-    private void HandleOff()
-    {
-        Helpers.CancelSchedule(_schedulerSubscription);
-        _schedulerSubscription = _scheduler.Schedule(
-            TimeSpan.FromMinutes(TurnBackOnInMinutes), 
-            () => _switches.HouseCameraNotificationsNetdaemon.TurnOn());
-    }
-
-    private void HandleOn()
-    {
-        Helpers.CancelSchedule(_schedulerSubscription);
+            .Subscribe(_ =>
+            {
+                Helpers.CancelSchedule(_schedulerSubscription);
+                _schedulerSubscription = _scheduler.Schedule(
+                    TimeSpan.FromMinutes(TurnBackOnInMinutes), 
+                    () =>
+                    {
+                        if (selects.HouseStateNetdaemon.State != RoomStates.HomeUnsecured.ToString())
+                        {
+                            _switches.HouseCameraNotificationsNetdaemon.TurnOn();
+                        }
+                    });
+            });
     }
 }
