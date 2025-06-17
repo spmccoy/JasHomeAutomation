26149c6a5d732b71f44b5002f6f02c659b93709e
spmccoy <48337525+spmccoy@users.noreply.github.com>
2025-06-17 17:57:53 -0400

    Add door event subscribers and corresponding use cases. (#43)
    Introduce a `DoorSubscriber` app to monitor door state changes and trigger appropriate use cases. Implement `DoorOpenedUseCase`, `DoorClosedUseCase`, `DoorLockedUseCase`, and `DoorUnLockedUseCase` to handle respective business logic and update the house state accordingly.

--- SmartHomeCore.Application/UseCases/DoorLockedUseCase.cs
+++ SmartHomeCore.Application/UseCases/DoorLockedUseCase.cs
@@ -1,25 +1,25 @@
 using Microsoft.Extensions.Logging;
 using SmartHomeCore.Application.Common;
+using SmartHomeCore.Domain.DoorEntity;
 using SmartHomeCore.Domain.HouseEntity;
-using SmartHomeCore.Domain.PersonEntity;
 
 namespace SmartHomeCore.Application.UseCases;
 
-public class PersonLeftHomeUseCase(House house, IDomainEventDispatcher domainEventDispatcher, ILogger<PersonLeftHomeUseCase> logger) 
+public class DoorLockedUseCase(House house, IDomainEventDispatcher domainEventDispatcher, ILogger<DoorLockedUseCase> logger)
     : UseCase(house, domainEventDispatcher, logger)
 {
     private readonly House _house = house;
-    private PersonId? PersonId { get; set; }
-
-    public Task HandleAsync(PersonId personId)
+    private DoorId? DoorId { get; set; }
+    
+    public Task HandleAsync(DoorId doorId)
     {
-        PersonId = personId ?? throw new ArgumentNullException(nameof(personId));
+        DoorId = doorId ?? throw new ArgumentNullException(nameof(doorId));
         return base.HandleAsync();
     }
     
     protected override Task ExecuteCoreLogicAsync()
     {
-        _house.MarkPersonAsAway(PersonId!);
+        _house.MarkDoorAsLocked(DoorId!);
         return Task.CompletedTask;
     }
 }
